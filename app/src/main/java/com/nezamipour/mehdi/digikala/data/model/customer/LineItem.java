582ba139a7e05017952f250a96b690165430e00a
Mehdi <mnezamipoor@gmail.com>
2020-12-09 22:19:59 +0330

    Add order to models for post orders of customer to server.

---
+++ app/src/main/java/com/nezamipour/mehdi/digikala/data/model/customer/LineItem.java
@@ -0,0 +1,90 @@
+
+package com.nezamipour.mehdi.digikala.data.model.customer;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+
+public class LineItem implements Serializable {
+
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("product_id")
+    @Expose
+    private Integer productId;
+    @SerializedName("variation_id")
+    @Expose
+    private Integer variationId;
+    @SerializedName("quantity")
+    @Expose
+    private Integer quantity;
+    @SerializedName("total")
+    @Expose
+    private String total;
+    @SerializedName("price")
+    @Expose
+    private Integer price;
+    private final static long serialVersionUID = -3493630255793866584L;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+    public Integer getVariationId() {
+        return variationId;
+    }
+
+    public void setVariationId(Integer variationId) {
+        this.variationId = variationId;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getTotal() {
+        return total;
+    }
+
+    public void setTotal(String total) {
+        this.total = total;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+}
---
+++ app/src/main/java/com/nezamipour/mehdi/digikala/data/model/customer/Order.java
@@ -0,0 +1,237 @@
+
+package com.nezamipour.mehdi.digikala.data.model.customer;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class Order implements Serializable {
+
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+    @SerializedName("parent_id")
+    @Expose
+    private Integer parentId;
+    @SerializedName("number")
+    @Expose
+    private String number;
+    @SerializedName("order_key")
+    @Expose
+    private String orderKey;
+    @SerializedName("status")
+    @Expose
+    private String status;
+    @SerializedName("currency")
+    @Expose
+    private String currency;
+    @SerializedName("discount_total")
+    @Expose
+    private String discountTotal;
+    @SerializedName("discount_tax")
+    @Expose
+    private String discountTax;
+    @SerializedName("shipping_total")
+    @Expose
+    private String shippingTotal;
+    @SerializedName("total")
+    @Expose
+    private String total;
+    @SerializedName("customer_id")
+    @Expose
+    private Integer customerId;
+    @SerializedName("customer_user_agent")
+    @Expose
+    private String customerUserAgent;
+    @SerializedName("customer_note")
+    @Expose
+    private String customerNote;
+    @SerializedName("billing")
+    @Expose
+    private Billing billing;
+    @SerializedName("shipping")
+    @Expose
+    private Shipping shipping;
+    @SerializedName("payment_method")
+    @Expose
+    private String paymentMethod;
+    @SerializedName("payment_method_title")
+    @Expose
+    private String paymentMethodTitle;
+    @SerializedName("transaction_id")
+    @Expose
+    private String transactionId;
+    @SerializedName("line_items")
+    @Expose
+    private List<LineItem> lineItems = null;
+    @SerializedName("currency_symbol")
+    @Expose
+    private String currencySymbol;
+
+
+    private final static long serialVersionUID = -8523793726844274382L;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(Integer parentId) {
+        this.parentId = parentId;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getOrderKey() {
+        return orderKey;
+    }
+
+    public void setOrderKey(String orderKey) {
+        this.orderKey = orderKey;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public String getDiscountTotal() {
+        return discountTotal;
+    }
+
+    public void setDiscountTotal(String discountTotal) {
+        this.discountTotal = discountTotal;
+    }
+
+    public String getDiscountTax() {
+        return discountTax;
+    }
+
+    public void setDiscountTax(String discountTax) {
+        this.discountTax = discountTax;
+    }
+
+    public String getShippingTotal() {
+        return shippingTotal;
+    }
+
+    public void setShippingTotal(String shippingTotal) {
+        this.shippingTotal = shippingTotal;
+    }
+
+    public String getTotal() {
+        return total;
+    }
+
+    public void setTotal(String total) {
+        this.total = total;
+    }
+
+    public Integer getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Integer customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerUserAgent() {
+        return customerUserAgent;
+    }
+
+    public void setCustomerUserAgent(String customerUserAgent) {
+        this.customerUserAgent = customerUserAgent;
+    }
+
+    public String getCustomerNote() {
+        return customerNote;
+    }
+
+    public void setCustomerNote(String customerNote) {
+        this.customerNote = customerNote;
+    }
+
+    public Billing getBilling() {
+        return billing;
+    }
+
+    public void setBilling(Billing billing) {
+        this.billing = billing;
+    }
+
+    public Shipping getShipping() {
+        return shipping;
+    }
+
+    public void setShipping(Shipping shipping) {
+        this.shipping = shipping;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public String getPaymentMethodTitle() {
+        return paymentMethodTitle;
+    }
+
+    public void setPaymentMethodTitle(String paymentMethodTitle) {
+        this.paymentMethodTitle = paymentMethodTitle;
+    }
+
+    public String getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(String transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public List<LineItem> getLineItems() {
+        return lineItems;
+    }
+
+    public void setLineItems(List<LineItem> lineItems) {
+        this.lineItems = lineItems;
+    }
+
+    public String getCurrencySymbol() {
+        return currencySymbol;
+    }
+
+    public void setCurrencySymbol(String currencySymbol) {
+        this.currencySymbol = currencySymbol;
+    }
+
+
+}
